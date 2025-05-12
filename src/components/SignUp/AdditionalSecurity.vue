d8987ff05d87f0e8117ad3a1315f3b417fbf3d88
aijprasetyo471 <ajiprsty4713@gmail.com>
2025-05-12 21:32:11 +0700

    finish fix 10 task 530

--- src/components/SignUp/AdditionalSecurity.vue
+++ src/components/SignUp/AdditionalSecurity.vue
@@ -15,17 +15,31 @@
             >
               <v-row>
                 <v-col cols="12">
-                <div class="text-center">
-                <h1 
-                :class="{ 'header-mobile-2': isSmall }" 
-                style="font-family: Arial, Helvetica, sans-serif !important">Basic Profile Setup</h1>
-                <h1 class="text-red-darken-4" 
-                :class="{ 'header-mobile-2': isSmall }" 
-                style="font-family: Arial, Helvetica, sans-serif !important">Step 1 of 5</h1>
-              </div>
-              <div style="height: 0.5px; background: black;" class="w-100 my-2"></div>
-            </v-col>
-                <v-col class="pb-16 pb-md-10" :cols="isSmall ? '12' : '6'">
+                  <div class="text-center">
+                    <h1
+                      :class="{ 'header-mobile-2': isSmall }"
+                      style="
+                        font-family: Arial, Helvetica, sans-serif !important;
+                      "
+                    >
+                      Basic Profile Setup
+                    </h1>
+                    <h1
+                      class="text-red-darken-4"
+                      :class="{ 'header-mobile-2': isSmall }"
+                      style="
+                        font-family: Arial, Helvetica, sans-serif !important;
+                      "
+                    >
+                      Step 1 of 5
+                    </h1>
+                  </div>
+                  <div
+                    style="height: 0.5px; background: black"
+                    class="w-100 my-2"
+                  ></div>
+                </v-col>
+                <v-col class="pb-16 pb-md-10" :cols="isSmall ? '12' : '4'">
                   <!-- <h1
                     class="mb-4"
                     style="font-family: Arial, Helvetica, sans-serif !important"
@@ -35,31 +49,34 @@
                   </h1> -->
 
                   <!-- <v-form fast-fail @submit.prevent="login"> -->
-                    <div class="d-flex align-center my-2">
+                  <div class="d-flex align-center my-2">
                     <p class="w-75">Birth Date</p>
                     <span class="w-33">Age</span>
                   </div>
-              <div class="d-flex align-center mt-2"> <input
-                v-model="date"
-                required
-                class="form-control w-66"
-                type="text"
-                id="dateInput"
-                placeholder="DD/MM/YYYY"
-                @input="onDateInput"
-              /> 
-              <span v-if="age" class="font-weight-bold ml-2 w-33"><span class="text-red">{{ age }}</span> Years</span>
-            </div>
-                    <p class="mb-2 mt-6">Marital Status</p>
-                    <VueMultiselect
-                  v-model="marital"
-                  class="mt-2 w-75"
-                  :options="resource.marital"
-                  track-by="value"
-                  label="title"
-                  placeholder="Select Marital Status"
-                />
-                    <!-- <v-autocomplete
+                  <div class="d-flex align-center mt-2">
+                    <input
+                      v-model="date"
+                      required
+                      class="form-control w-66"
+                      type="text"
+                      id="dateInput"
+                      placeholder="DD/MM/YYYY"
+                      @input="onDateInput"
+                    />
+                    <span v-if="age" class="font-weight-bold ml-2 w-33"
+                      ><span class="text-red">{{ age }}</span> Years</span
+                    >
+                  </div>
+                  <p class="mb-2 mt-6">Marital Status</p>
+                  <VueMultiselect
+                    v-model="marital"
+                    class="mt-2 w-75"
+                    :options="resource.marital"
+                    track-by="value"
+                    label="title"
+                    placeholder="Select Marital Status"
+                  />
+                  <!-- <v-autocomplete
                       v-model="city"
                       :items="resource.cities"
                       label="Enter City"
@@ -77,63 +94,65 @@
                       clearable
                     /> -->
 
-                    <div
+                  <div
                     class="d-flex align-center"
                     :class="{ matop: !isSmall, 'fixed-next w-100': isSmall }"
                   >
-                  <v-container class="d-flex justify-space-between align-center" v-if="isSmall">
+                    <v-container
+                      class="d-flex justify-space-between align-center"
+                      v-if="isSmall"
+                    >
+                      <v-btn
+                        type="submit"
+                        variant="outlined"
+                        class="login-btn"
+                        :class="{
+                          'w-33 login-btn-mobile': isSmall,
+                          'w-25': !isSmall,
+                        }"
+                        @click="saveData"
+                      >
+                        Next
+                      </v-btn>
+                      <!-- <div
+                      class="text-blue-darken-4"
+                      :class="{
+                        'w-33 login-btn-mobile': isSmall,
+                        'w-25': !isSmall,
+                      }"
+                      style="
+                        text-align: center;
+                        cursor: pointer;
+                        font-weight: 700;
+                        font-size: 20px;
+                      "
+                      @click="backStep"
+                    >
+                      Back
+                    </div> -->
+                    </v-container>
                     <v-btn
+                      v-if="!isSmall"
                       type="submit"
                       variant="outlined"
                       class="login-btn"
                       :class="{
                         'w-33 login-btn-mobile': isSmall,
-                        'w-25': !isSmall,
+                        'w-25 mt-6': !isSmall,
                       }"
                       @click="saveData"
                     >
                       Next
                     </v-btn>
-                    <!-- <div
-                  class="text-blue-darken-4"
-                  :class="{
-                    'w-33 login-btn-mobile': isSmall,
-                    'w-25': !isSmall,
-                  }"
-                  style="
-                    text-align: center;
-                    cursor: pointer;
-                    font-weight: 700;
-                    font-size: 20px;
-                  "
-                  @click="backStep"
-                >
-                  Back
-                </div> -->
-                  </v-container>
-                  <v-btn
-                  v-if="!isSmall"
-                  type="submit"
-                  variant="outlined"
-                  class="login-btn"
-                  :class="{
-                    'w-33 login-btn-mobile': isSmall,
-                    'w-25 mt-6': !isSmall,
-                  }"
-                  @click="saveData"
-                >
-                  Next
-                </v-btn>
                   </div>
                   <!-- </v-form> -->
                 </v-col>
                 <v-col
                   v-if="!isSmall"
-                  cols="6"
+                  cols="4"
                   class="d-flex align-center justify-center"
                 >
-                  <h1 style="width: 80%">
-                  </h1>
+                  <h1 style="width: 80%"></h1>
                 </v-col>
               </v-row>
             </v-card>
@@ -159,12 +178,12 @@
 </template>
 
 <script>
-import VueMultiselect from "vue-multiselect";
-import "vue-multiselect/dist/vue-multiselect.css";
+import VueMultiselect from 'vue-multiselect';
+import 'vue-multiselect/dist/vue-multiselect.css';
 import axios from '@/util/axios';
 export default {
   // eslint-disable-next-line vue/multi-word-component-names
-  name: 'WhereAreYou',  
+  name: 'WhereAreYou',
   components: {
     VueMultiselect,
   },
@@ -180,8 +199,8 @@ export default {
           // "Single",
           // "Married",
           // "Others",
-          { title: "Single", value: "S" },
-          { title: "Married", value: "M" },
+          { title: 'Single', value: 'S' },
+          { title: 'Married', value: 'M' },
         ],
       },
     };
@@ -193,7 +212,7 @@ export default {
     age() {
       if (!this.date) return null;
 
-      const [day, month, year] = this.date.split("/").map(Number);
+      const [day, month, year] = this.date.split('/').map(Number);
       if (isNaN(day) || isNaN(month) || isNaN(year)) return null;
 
       const today = new Date();
@@ -220,7 +239,7 @@ export default {
   },
   methods: {
     onDateInput() {
-      const formattedDate = this.date.replace(/\D/g, "");
+      const formattedDate = this.date.replace(/\D/g, '');
 
       // Memisahkan tanggal, bulan, dan tahun
       const day = formattedDate.substring(0, 2);
@@ -230,7 +249,7 @@ export default {
       // Memeriksa apakah tanggal, bulan, dan tahun valid
       if (day && month && year) {
         // Mengatur format tanggal yang sesuai dengan JavaScript (mm/dd/yyyy)
-        this.date = `${month}/${day}/${year}`;
+        this.date = `${day}/${month}/${year}`;
       } else {
         this.date = formattedDate; // Tidak valid, tetapkan nilai yang sama
       }
@@ -242,33 +261,35 @@ export default {
         marital_status: this.marital.value,
       };
       console.log(payload);
-      const token = localStorage.getItem("token");
-      if(this.date && this.marital) {
-      axios
-        .post(`/gypsy-applicant/save-date-of-birth-and-marital-status`, payload, {
-          headers: {
-            Authorization: `Bearer ${
-              token
-            }`,
-          },
-        })
-        .then((response) => {
-          const data = response.data;
-          console.log(data)
-          this.isSuccess = true;
-          this.successMessage = data.message;
-          this.nextStep()
-        })
-        .catch((error) => {
-          // eslint-disable-next-line
-          console.log(error);
-          const message =
-            error.response.data.message === ""
-              ? "Something Wrong!!!"
-              : error.response.data.message;
-          this.errorMessage = message;
-          this.isError = true;
-        })
+      const token = localStorage.getItem('token');
+      if (this.date && this.marital) {
+        axios
+          .post(
+            `/gypsy-applicant/save-date-of-birth-and-marital-status`,
+            payload,
+            {
+              headers: {
+                Authorization: `Bearer ${token}`,
+              },
+            }
+          )
+          .then((response) => {
+            const data = response.data;
+            console.log(data);
+            this.isSuccess = true;
+            this.successMessage = data.message;
+            this.nextStep();
+          })
+          .catch((error) => {
+            // eslint-disable-next-line
+            console.log(error);
+            const message =
+              error.response.data.message === ''
+                ? 'Something Wrong!!!'
+                : error.response.data.message;
+            this.errorMessage = message;
+            this.isError = true;
+          });
       }
     },
     nextStep() {
@@ -327,7 +348,7 @@ export default {
   position: fixed;
   bottom: 0;
   left: 0;
-  background: #F3F3F3;
+  background: #f3f3f3;
 }
 .login-btn {
   width: 400px;
